30f4fb5b9c8f00e31e99b7089adcd76e0488afad
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2021-03-04 16:23:03 +0100

    New Branch

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,7 @@
+namespace Drip.Controllers
+{
+    public class LoginController
+    {
+        
+    }
+}
---
+++ Views/Home/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Drip.Views.Home
+{
+    public class Test : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
